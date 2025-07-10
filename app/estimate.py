8fc938ffee1c539db9f3665c44e013c1d6b8ae53
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-10 07:36:17 +0000

    feat: version

--- app/estimate.py
+++ app/estimate.py
@@ -2,7 +2,10 @@ from sagemaker.sklearn.estimator import SKLearn
 
 estimator = SKLearn(
     entry_point="train.py",
-    instance_type="local"
+    # ml.t3.medium, ml.m4.xlarge
+    instance_type="local",
+    framework_version="1.2-1",
+    hyperparameters={}
 )
 
 estimator.fit()
