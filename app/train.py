aa705fe1c4facfc5efb142bc7e69ba731a369db9
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-10 07:35:49 +0000

    feat: load env

--- app/train.py
+++ app/train.py
@@ -1,7 +1,15 @@
 import argparse
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
+    parser.add_argument("--output-data-dir", type=str, default=os.environ.get("SM_OUTPUT_DATA_DIR"))
+    parser.add_argument("--model-dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
+    parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
+    parser.add_argument("--test", type=str, default=os.environ.get("SM_CHANNEL_TEST"))
 
     args = parser.parse_args()
