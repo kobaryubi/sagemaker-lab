f5446c319083af85954239a7875a0317e1a6591d
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-10 08:50:30 +0000

    feat: session

--- app/estimate.py
+++ app/estimate.py
@@ -1,11 +1,17 @@
+import boto3
 from sagemaker.sklearn.estimator import SKLearn
+from sagemaker.local import LocalSession
+
+boto_session = boto3.Session(region_name="ap-northeast-1")
+sagemaker_session = LocalSession(boto_session=boto_session)
 
 estimator = SKLearn(
     entry_point="train.py",
     # ml.t3.medium, ml.m4.xlarge
     instance_type="local",
     framework_version="1.2-1",
-    hyperparameters={}
+    hyperparameters={},
+    sagemaker_session=sagemaker_session,
 )
 
 estimator.fit()
