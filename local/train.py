98c956530002f78c3933aaad9b9f78d8e0b0cd6e
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-11 18:27:05 +0900

    feat: model fn

--- local/train.py
+++ local/train.py
@@ -17,7 +17,12 @@ if __name__ == "__main__":
     train_y = train_data.iloc[:, 0]
     train_X = train_data.iloc[:, 1:]
 
-    clf = tree.DecisionTreeClassifier(max_leaf_nodes=2)
+    clf = tree.DecisionTreeClassifier(max_leaf_nodes=30)
     clf = clf.fit(train_X, train_y)
 
     joblib.dump(clf, os.path.join(args.model_dir, "model.joblib"))
+
+
+def model_fn(model_dir):
+    clf = joblib.load(os.path.join(model_dir, "model.joblib"))
+    return clf
