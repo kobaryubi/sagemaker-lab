9dd4817fd48b7f232731ce5ee8b4188b8ac958af
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-11 15:09:42 +0900

    feat: train

--- app/train.py
+++ app/train.py
@@ -1,8 +1,8 @@
 import argparse
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import pandas as pd
+from sklearn import tree
+import joblib
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -13,3 +13,15 @@ if __name__ == "__main__":
     parser.add_argument("--test", type=str, default=os.environ.get("SM_CHANNEL_TEST"))
 
     args = parser.parse_args()
+
+    input_files = [ os.path.join(args.train, file) for file in os.listdir(args.train) ]
+    raw_data = [ pd.read_csv(file, header=None, engine="python") for file in input_files ]
+    train_data = pd.concat(raw_data)
+
+    train_y = train_data.iloc[:, 0]
+    train_X = train_data.iloc[:, 1:]
+
+    clf = tree.DecisionTreeClassifier(max_leaf_nodes=2)
+    clf = clf.fit(train_X, train_y)
+
+    joblib.dump(clf, os.path.join(args.model_dir, "model.joblib"))
