9565fb925100c8df7481504dfe2d2f5896d479f3
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-11 15:55:20 +0900

    feat: estimate

---
+++ local/estimate.py
@@ -0,0 +1,18 @@
+from sagemaker.sklearn.estimator import SKLearn
+from sagemaker.local import LocalSession
+
+sagemaker_session = LocalSession()
+sagemaker_session.config = {"local": {"local_code": True}}
+
+estimator = SKLearn(
+    entry_point="./local/train.py",
+    instance_type="local",
+    framework_version="1.2-1",
+    hyperparameters={},
+    sagemaker_session=sagemaker_session,
+    role="lab-sagemaker-execution-role"
+)
+
+estimator.fit({
+    "train": "file://data/train",
+})
